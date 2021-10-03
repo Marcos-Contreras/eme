80d679728cf171b22a2bcde1a3679e69b9429a24
MonserratHC <hrdzcont@gmail.com>
2021-10-03 13:22:20 -0500

    borrado modelo category a productcontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\Provider;
+
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Requests\Product\StoreRequest;

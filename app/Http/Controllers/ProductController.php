36eee66df6df34e2673df8c037d38b77e322c3d9
MonserratHC <hrdzcont@gmail.com>
2021-10-05 09:36:13 -0500

    borrados modelos de salecontroller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Models\Provider;
-use App\Models\Category;
+
 use Illuminate\Http\Request;
 use App\Http\Requests\Product\StoreRequest;
 use App\Http\Requests\Product\UpdateRequest;

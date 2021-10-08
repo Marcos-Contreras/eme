092dc6864a247988faa4f2eadc7b0e5823331d8a
Marcos Andres Alonso Contreras <marcosalonso2230@gmail.com>
2021-10-08 10:44:12 -0500

    Agregada plantilla back y front con rutas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//VISTA DEL CLIENTE
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/prueba', function () {
+    return view('layouts.prueba');
+});
+
+Route::get('/productos', function () {
+    return view('web.shop_grid');
+});
+
+Route::get('/detalles', function () {
+    return view('web.product_details');
+});
+
+Route::get('/micuenta', function () {
+    return view('web.my_account');
+});
+
+Route::get('/registro', function () {
+    return view('web.login_register');
+});
+
+Route::get('/contacto', function () {
+    return view('web.contact_us');
+});
+
+Route::get('/carrito', function () {
+    return view('web.cart');
+});
+
+Route::get('/nosotros', function () {
+    return view('web.about_us');
+});
+
+Route::get('/pagar', function () {
+    return view('web.checkout');
+});
+
+
+//VISTA DEL ADMINISTRADOR
+//Route::resource('categories', 'CategoryController')->names('categories');
+Route::resource('/categories', CategoryController::class)->names('categories');
+//Route::resource('clients', 'ClientController')->names('clients');
+Route::resource('/clients', ClientController::class)->names('clients');
+//Route::resource('products', 'ProductController')->names('products');
+Route::resource('/products', ProductController::class)->names('products');
+//Route::resource('providers', 'Providerontroller')->names('providers');
+Route::resource('/providers', Providerontroller::class)->names('providers');
+//Route::resource('sales', 'SaleController')->names('sales');
+Route::resource('/sales', SaleController::class)->names('sales');
